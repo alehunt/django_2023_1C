eb66446f7e8851d35a95770326c1aeff6fec6f14
alehunt <alehunt@gmail.com>
2023-03-28 20:21:34 -0300

    Proyecto Django creado en vivo y plan de estudio actualizado

---
+++ Reunion_09/sistema_gestion/ventas/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class VentasConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ventas'
---
+++ Reunion_09/sistema_gestion/ventas/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+# def hola(request):
+#     variable = "hola"
+#     return "hola"
