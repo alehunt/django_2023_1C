42a7eb3c4d8da2ef971760595a4e312f573805a1
alehunt <alehunt@gmail.com>
2023-05-11 20:36:21 -0300

    Reunion_22, nuevas migraciones inicializar base de 0, y django_extensions

--- pig_23320/administracion/models.py
+++ pig_23320/administracion/models.py
@@ -1,34 +1,148 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+# Modelo UNICO - SOLUCION 1
+# class PersonaU(models.Model):
+#     nombre = models.CharField(max_length=100,verbose_name='Nombre')
+#     apellido = models.CharField(max_length=150,verbose_name='Apellido')
+#     email = models.EmailField(max_length=150,null=True)
+#     dni = models.IntegerField(verbose_name="DNI")
+#     matricula = models.CharField(max_length=10,verbose_name='Matricula',null=True)
+#     baja = models.BooleanField(default=0,null=True)
+#     legajo = models.CharField(max_length=10,verbose_name='Legajo',null=True)
+
+# Modelo Abtracto - SOLUCION 2
+
+
+class PersonaAbs(models.Model):
+    nombre = models.CharField(max_length=100, verbose_name='Nombre')
+    apellido = models.CharField(max_length=150, verbose_name='Apellido')
+    email = models.EmailField(max_length=150, null=True)
+    dni = models.IntegerField(verbose_name="DNI")
+    edad = models.IntegerField(verbose_name="Edad", default=0)
+
+    class Meta:
+        abstract = True
+
+
+class EstudianteAbs(PersonaAbs):
+    matricula = models.CharField(max_length=10, verbose_name='Matricula')
+    baja = models.BooleanField(default=0, null=True)
+
+
+class InstructorAbs(PersonaAbs):
+    legajo = models.CharField(max_length=10, verbose_name='Legajo')
+
+# HERENCIA - SOLUCION 3
+
+
+class Persona(models.Model):
+    nombre = models.CharField(max_length=100, verbose_name='Nombre')
+    apellido = models.CharField(max_length=150, verbose_name='Apellido')
+    email = models.EmailField(max_length=150, null=True)
+    dni = models.IntegerField(verbose_name="DNI")
+
+
+class Estudiante(Persona):
+    matricula = models.CharField(max_length=10, verbose_name='Matricula')
+    baja = models.BooleanField(default=0)
+
+    def __str__(self):
+        return f"{self.matricula} - {self.nombre} {self.apellido}"
+
+    def soft_delete(self):
+        self.baja = True
+        super().save()
+
+    def restore(self):
+        self.baja = False
+        super().save()
+
+    class Meta():
+        verbose_name_plural = 'Estudiantes'
+        # db_table = 'nombre_tabla'
+
+
+class Instructor(Persona):
+    legajo = models.CharField(max_length=10, verbose_name='Legajo')
+
+
 class Categoria(models.Model):
-    nombre = models.CharField(max_length=50,verbose_name='Nombre')
+    nombre = models.CharField(max_length=50, verbose_name='Nombre')
     baja = models.BooleanField(default=0)
 
     def __str__(self):
         return self.nombre
 
     def soft_delete(self):
-        self.baja=True
-        super().save()
-    
-    def restore(self):
-        self.baja=False
+        self.baja = True
         super().save()
 
+    def restore(self):
+        self.baja = False
+        super().save()
+
+
 class Curso(models.Model):
-    nombre = models.CharField(max_length=100,verbose_name='Nombre')
-    descripcion = models.TextField(null=True,verbose_name='Descripcion')
-    fecha_inicio = models.DateField(verbose_name='Fecha de inicio',null=True,default=None)
-    portada = models.ImageField(upload_to='imagenes/',null=True,verbose_name='Portada')
-    categoria = models.ForeignKey(Categoria,on_delete=models.CASCADE) #relacion mucho a uno    
+    nombre = models.CharField(max_length=100, verbose_name='Nombre')
+    descripcion = models.TextField(null=True, verbose_name='Descripcion')
+    fecha_inicio = models.DateField(verbose_name='Fecha de inicio', null=True, default=None)
+    portada = models.ImageField(upload_to='imagenes/', null=True, verbose_name='Portada')
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)  # relacion mucho a uno
 
     def __str__(self):
         return self.nombre
-    
-    def delete(self,using=None,keep_parents=False):
-        self.portada.storage.delete(self.portada.name) #borrado fisico
+
+    def delete(self, using=None, keep_parents=False):
+        self.portada.storage.delete(self.portada.name)  # borrado fisico
         super().delete()
 
-# CONSIGNA DE TAREA: Crear el modelo de Comisi�n en base a estos ejemplos vistos y teniendo en 
-# cuenta el diagrama de clases.
+
+class Comision(models.Model):
+    nombre = models.CharField(max_length=100, verbose_name="Nombre")
+    horario = models.CharField(max_length=100, verbose_name="Horario", null=True, default=None)
+    link_meet = models.URLField(max_length=100, verbose_name='Link de Meet')
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)  # relacion mucho a uno
+    estudiantes = models.ManyToManyField(Estudiante, through='Inscripcion')
+
+# Modelo que genera tabla intermedia automaticamente
+# class ComisionMTM(models.Model):
+#     nombre = models.CharField(max_length=100,verbose_name="Nombre")
+#     horario = models.CharField(max_length=100,verbose_name="Horario",null=True,default=None)
+#     link_meet = models.URLField(max_length=100,verbose_name='Link de Meet')
+#     curso = models.ForeignKey(Curso,on_delete=models.CASCADE) #relacion mucho a uno
+#     estudiantes = models.ManyToManyField(Estudiante)
+
+
+class Inscripcion(models.Model):
+
+    # ESTADOS = [
+    #     (1, 'Inscripto'),
+    #     (2, 'Cursando'),
+    #     (3, 'Egresado'),
+    # ]
+
+    # Opción mas actual con clase choice
+    class Estado(models.TextChoices):
+        INSCRIPTO = 'INS', 'Inscripto'
+        CURSANDO = 'CUR', 'Cursando'
+        EGRESADO = 'EGR', 'Egresado'
+
+    fecha_creacion = models.DateField(auto_now_add=True, verbose_name='Fecha de creacion')
+    estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
+    comision = models.ForeignKey(Comision, on_delete=models.CASCADE)
+    # estado = models.IntegerField(choices=ESTADOS, default=1)
+    # Opción mas actual con clase choice
+    estado = models.CharField(max_length=3, choices=Estado.choices, default=Estado.INSCRIPTO)
+
+    def __str__(self):
+        return self.estudiante.nombre
+
+
+class Perfil(models.Model):
+    user = models.OneToOneField(Persona, on_delete=models.CASCADE)
+    telefono = models.CharField(max_length=20, verbose_name='Teléfono')
+    domicilio = models.CharField(max_length=20, verbose_name='Domicilio')
+    foto = models.ImageField(upload_to='perfiles/', null=True, verbose_name='Foto Perfil')
--- pig_23320/pig_23320/settings.py
+++ pig_23320/pig_23320/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'portal',
     'administracion',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
